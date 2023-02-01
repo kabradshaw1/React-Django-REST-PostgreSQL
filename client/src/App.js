d3d76975df28a0cce5faebe8c1ce1ab42e23b18c
kabradshaw1 <itllbok82@gmail.com>
2023-02-01 11:56:06 -0500

    server api work with most of the settings in place for heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
-
+import Container from 'react-bootstrap/Container';
+import {useState} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert'
 
 function App() {
   return (
--- server/server/settings.py
+++ server/server/settings.py
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     'https://localhost:3000',
-    'serene-tor-73175.herokuapp.com',
+    'http://serene-tor-73175.herokuapp.com',
 ]
 
 django_heroku.settings(locals())
