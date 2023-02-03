58c35fd06a04e712d8d860ff10d021a2c299baba
kabradshaw1 <itllbok82@gmail.com>
2023-02-03 12:21:19 -0500

    corrected wrong line commented out

--- server/urls.py
+++ server/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from core.views import UserViewSet
-# from menu.views import MenuViewSet
+# from core.views import UserViewSet
+from menu.views import MenuViewSet
 
 router = routers.DefaultRouter()
 # router.register(r'users', UserViewSet, 'user')
