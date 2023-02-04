f4026047146a15a089652fd798aa31bf1d23135a
kabradshaw1 <itllbok82@gmail.com>
2023-02-04 10:29:22 -0500

    added middleware to fix bug in development start up of react server

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,34 @@
+
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import  Link  from 'react-router-dom/Link';
+
+
+function Header() {
+  
+  return(
+    <Container fluid>
+      <Row className='align-items-center'>
+        <Col>Working App Name</Col>
+        <Col>
+          <Nav variant='pills'>
+            {/* <Nav.Item>
+              <Nav.Link as={Link} to={'/add/'}>
+                Add
+              </Nav.Link>
+            </Nav.Item> */}
+            <Nav.Item>
+              <Nav.Link>
+                Place Holder
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Header;
