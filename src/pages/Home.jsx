d0cdb8de8fd75ffaea1654702e5caf8c352c8123
kabradshaw1 <itllbok82@gmail.com>
2023-02-06 19:56:08 -0500

    stuff is going wierd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Header from './components/Header';
-import MenuAdd from './components/MenuAdd';
 
-function Home() {
-  return (
-    <>
-      <Header/>
-      <Container>
-        <Row>
-            <MenuAdd as={Col}/>
-            <MenuGet as={Col}/>
-        </Row>
-      </Container>
-    </>
-  );
-}
+// import Container from 'react-bootstrap/Container';
+// import Col from 'react-bootstrap/Col';
+// import Row from 'react-bootstrap/Row';
+// import Header from './components/Header';
+// import MenuAdd from './components/MenuAdd';
 
-export default Home;
+// function Home() {
+//   return (
+//     <>
+//       <Header/>
+//       <Container>
+//         <Row>
+//             <MenuAdd as={Col}/>
+//             <MenuGet as={Col}/>
+//         </Row>
+//       </Container>
+//     </>
+//   );
+// }
+
+// export default Home;
