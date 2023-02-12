7864ad55120455d936307db52379f54af33bb694
kabradshaw1 <itllbok82@gmail.com>
2023-02-12 09:51:04 -0500

    added repsonse to model and serializer

--- post/models.py
+++ post/models.py
@@ -12,7 +12,13 @@ class Post(models.Model):
 class Response(models.Model):
     responseText = models.CharField(max_length=255)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
 
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
 
--- post/serializers.py
+++ post/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from post.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    response = serializers.StringRelatedField(many=true)
+
     class Meta:
         model = Post
-        fields = ['id', 'postText', 'username', 'created', 'updated']  
+        fields = ['id', 'postText', 'username', 'created', 'updated', 'response']  
