54aa830905bc44639ca7af6fcc68c1de1a3e6568
kabradshaw1 <itllbok82@gmail.com>
2023-02-13 07:25:39 -0500

    added a bit to like

--- post/models.py
+++ post/models.py
@@ -21,7 +21,7 @@ class Response(models.Model):
 
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
+    response = models.ForeignKey(Response, on_delete=models.CASCADE)
 
 
 
