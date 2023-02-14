59401e3447f5465c8cd6d0210f75b2bea8dcff6c
kabradshaw1 <itllbok82@gmail.com>
2023-02-14 15:58:36 -0500

    created home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import Header from './components/Header';
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
-import Login from './components/Login';
-import RegiserUser from './components/RegisterUser';
+import { Provider } from 'react-redux';
+import store from './store'
+import Login from './page/login';
+import RegiserUser from './page/RegisterUser';
+import Hume from './page/Home'
 
 const App: React.FC = () => {
   return(
     <>
       <Header/>
-      <BrowserRouter>
-        <Routes>
-          {/* <Route path="/" element={<Home/>}/> */}
-          <Route path="/Register" element={<RegiserUser/>}/>
-          <Route path="/Login" element={<Login/>}/>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/Register" element={<RegiserUser/>}/>
+            <Route path="/Login" element={<Login/>}/>
+          </Routes>
+        </BrowserRouter>
+        </Provider>
     </>
   )
 };
