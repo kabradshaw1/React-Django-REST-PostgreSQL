ee940c4528ad19b80ba68364954dd95f47adb9ce
kabradshaw1 <itllbok82@gmail.com>
2023-02-14 15:07:51 -0500

    routes defined

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-import Header from "./components/Header";
-import { Routes , Route } from 'react-router-dom';
+import React from 'react';
+import Header from './components/Header';
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import Login from './components/Login';
+import RegiserUser from './components/RegisterUser';
 
 const App: React.FC = () => {
   return(
-    <Header/>
+    <>
+      <Header/>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Home/>}/> */}
+          <Route path="/Register" element={<RegiserUser/>}/>
+          <Route path="/Login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 };
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,15 +6,15 @@ import Navbar from 'react-bootstrap/Navbar';
 const Header: React.FC = () => {
   return(
     <Navbar bg="dark" variant="dark">
-    <Container>
-      <Navbar.Brand>Kyle's Social Media</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link>Home</Nav.Link>
-        <Nav.Link>Login</Nav.Link>
-        <Nav.Link>Register</Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>
+      <Container>
+        <Navbar.Brand>Kyle's Social Media</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link>Home</Nav.Link>
+          <Nav.Link>Login</Nav.Link>
+          <Nav.Link>Register</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   )
 }
 
