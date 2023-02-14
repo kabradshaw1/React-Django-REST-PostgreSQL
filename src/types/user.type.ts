316ada339d80ba81d8c26f0e63e6be27e0d985cc
kabradshaw1 <itllbok82@gmail.com>
2023-02-14 13:41:46 -0500

    currently all example code removed

--- src/types/user.type.ts
+++
@@ -1,6 +0,0 @@
-export default interface IUser {
-  id?: any | null,
-  email: string,
-  password: string,
-  roles?: Array<string>
-}
--- src/utils/types.ts
+++
@@ -1,8 +0,0 @@
-export interface UserResponse {
-  email: string;
-  username: string;
-  is_active: string;
-  created: Date;
-  updated: Date;
-  id: string;
-}
