ff6126eb7e70e3c7e1b59ae8b8b2a1d77a2fe9f1
kabradshaw1 <itllbok82@gmail.com>
2023-02-16 13:24:11 -0500

    working on user test

--- social/user/tests.py
+++ social/user/tests.py
@@ -3,4 +3,12 @@ from social.user.model import User, UserManager
 
 class UserTestCase(TestCase):
   def setUp(self):
-    User.objects.create(email="test@mail.com", is_active="True", is_staff="True",)
+    # I need to figure out how to create tests for objects that are inherited.  
+    # Could be possible that the best approach is to think of this test as if I
+    # I can't already see what the unit I'm attempting to test looks like finished.
+    User.objects.create(email="test@mail.com", is_active="True", is_staff="True", object='needs help')
+
+  def test_user(self):
+    email = User.objects.get(name="test@mail.com")
+    is_active = User.objects.get(name="True")
+    
