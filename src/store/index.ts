3565042792b3116887984e2a2c2df8ebc8d67922
kabradshaw1 <itllbok82@gmail.com>
2023-02-16 07:42:12 -0500

    added a temperary react-typescript-redux app

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+// This package is to make setting up easier...  Aparently
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
+// This is going to privide access to global states
+
+const store = configureStore()
