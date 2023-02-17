1ba18e4231bf6f8b7d33bd3b2650e9601d3856ba
kabradshaw1 <itllbok82@gmail.com>
2023-02-17 09:37:12 -0500

    working on post test

--- social/post/models.py
+++ social/post/models.py
@@ -3,7 +3,6 @@ from social.user.models import User
 
 class Post(models.Model):
     postText = models.CharField(max_length=255)
-    username = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
--- social/post/tests.py
+++ social/post/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from social.post.models import Post
+from social.response.models import Response
+from social.like.models import Like
+from social.user.models import User
 
-# Create your tests here.
+class TestPostModels(TestCase):
+  def test_post_has_many_responses(self):
+    Post = Book.objects.create(postText='This is a test post', username='Kyle', )
