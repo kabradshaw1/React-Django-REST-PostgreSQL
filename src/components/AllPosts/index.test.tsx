6bb4410ccdba2956de3c67b7412d561c9e6f5f14
kabradshaw1 <itllbok82@gmail.com>
2023-02-21 17:53:01 -0500

    writing tests

---
+++ src/components/AllPosts/index.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {} from "./index";
+
+test('Should do a get request for all posts', () => {
+  
+});
+
+test('Should render post component', () => {
+
+});
---
+++ src/page/Home/index.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Home from './index';
+
+test('Home page should have a list of all posts', () => {
+  
+});
+
