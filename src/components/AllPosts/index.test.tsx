10353bc0895aefdede228740281743e380d17eb7
kabradshaw1 <itllbok82@gmail.com>
2023-02-23 13:51:30 -0500

    writing getPosts test

--- src/components/AllPosts/index.test.tsx
+++ src/components/AllPosts/index.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AllPosts, { getPosts } from "./index";
+import AllPosts from "./index";
 import { render } from '@testing-library/react'
 import axios from "axios";
 
@@ -8,10 +8,14 @@ const mockedAxios = axios as jest.Mocked<typeof axios>
 
 describe('API call test', () => {
   describe('when API call is successful', () => {
+
+    const renderComponent = () => (render(<AllPosts))
     test('Should do a get request for all posts', async () => {
-      axios.get.mockResolvedValue()
-      const post = await getPosts;
-      expect(post).toEqual('First Post')
+      axios.get.mockResolvedValue({
+        data: []
+      })
+    
+      // expect(post).toEqual('First Post')
     })
   });
 
