9ab0460dcca56f290d889ce247a780d7f044c09c
kabradshaw1 <itllbok82@gmail.com>
2023-02-23 12:30:42 -0500

    trying to write typescript tests for get posts

--- src/components/AllPosts/index.test.tsx
+++ src/components/AllPosts/index.test.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { getPreEmitDiagnostics } from "typescript";
 import AllPosts, { getPosts } from "./index";
-
-const axios = require('axios');
+import axios from "axios";
 
 jest.mock('axios');
 
-test('Should do a get request for all posts', async () => {
-  const post = await getPosts;
-  expect(post).toEqual('First Post')
-});
+describe('API call test', () => {
+  test('Should do a get request for all posts', async () => {
+    axios.get.mockResolvedValue( {
+      {
+
+      }
+    })
+    const post = await getPosts;
+    expect(post).toEqual('First Post')
+  });
+})
 
 test('Should render post component', () => {
 
