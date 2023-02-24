f02ad0aa6befccf94b5ed76503bbaccb9c5308f6
kabradshaw1 <itllbok82@gmail.com>
2023-02-24 18:38:35 -0500

    working on server models

--- social/response/models.py
+++ social/response/models.py
@@ -5,7 +5,7 @@ from social.user.models import User
 class Response(models.Model):
     responseText = models.CharField(max_length=255)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, default="", on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
